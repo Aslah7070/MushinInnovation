dcf2ea0de6814345d473c19f5a2f724cd4ba9c0c
Aslah7070 <aslah.c7070@gmail.com>
2025-09-08 15:28:19 +0530

    issue

--- client/src/layout/Main.tsx
+++ client/src/layout/Main.tsx
@@ -1,14 +1,11 @@
 // src/layouts/MainLayout.tsx
-import { Navigate, Outlet } from "react-router-dom";
+
 import NavBar from "../components/navbar/NavBar";
-import { useAppSelector } from "../redux/hooks/hook";
+
 
 
 export default function MainLayout() {
-    const { isAuthenticated } = useAppSelector((state) => state.auth);
-  if (isAuthenticated ) {
-    return <Navigate to="/home" replace />;
-  }
+
   return (
     <>
       <NavBar />
